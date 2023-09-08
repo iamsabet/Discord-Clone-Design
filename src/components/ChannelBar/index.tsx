36fda0f6e08b287d2f286be665c0068bc67c3598
iamsabet <iamsabet@gmail.com>
2023-09-08 20:13:03 +0330

    format on save settings

--- src/components/ChannelBar/index.tsx
+++ src/components/ChannelBar/index.tsx
@@ -1,73 +1,81 @@
-import { useState } from 'react';
-import { BsHash } from 'react-icons/bs';
-import { FaChevronDown, FaChevronRight, FaPlus } from 'react-icons/fa';
-
-const topics = ['Resident Evil', 'react'];
-const questions = ['jit-compilation', 'purge-files', 'dark-mode'];
-const random = ['variants', 'plugins'];
+import { useState } from "react";
+import { FaChevronDown, FaChevronRight, FaPlus } from "react-icons/fa";
+import { RiHashtag } from "react-icons/ri";
+const topics = ["Resident Evil", "react"];
+const questions = ["jit-compilation", "purge-files", "dark-mode"];
+const random = ["variants", "plugins"];
 
 const ChannelBar = () => {
-    return (
-        <div className='channel-bar'>
-            <ChannelBlock />
-            <div className='channel-container'>
-                <Dropdown header='Topics' selections={topics} />
-                <Dropdown header='Questions' selections={questions} />
-                <Dropdown header='Random' selections={random} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="channel-bar">
+      <ChannelBlock />
+      <div className="channel-container">
+        <Dropdown header="Topics" selections={topics} />
+        <Dropdown header="Questions" selections={questions} />
+        <Dropdown header="Random" selections={random} />
+      </div>
+    </div>
+  );
+};
 
 interface DropPropsType {
-    header:string; 
-    selections:string[];
+  header: string;
+  selections: string[];
 }
 
-const Dropdown = ({header, selections} : DropPropsType) => {
-    const [expanded, setExpanded] = useState(true);
+const Dropdown = ({ header, selections }: DropPropsType) => {
+  const [expanded, setExpanded] = useState(true);
 
-    return (
-        <div className='dropdown'>
-            <div onClick={()=> setExpanded(!expanded)} className='dropdown-header group'>
-                <ChevronIcon expanded={expanded} />
-                <h5 className={'dropdown-header-text group-hover:text-gray-300'}
-                >
-                    {header}
-                </h5>
-                <FaPlus size='12' className='text-accent text-opacity-90 ml-auto my-auto' />
-            </div>
+  return (
+    <div className="dropdown">
+      <div
+        onClick={() => setExpanded(!expanded)}
+        className="dropdown-header group"
+      >
+        <ChevronIcon expanded={expanded} />
+        <h5
+          className={
+            "dropdown-header-text group-hover:text-gray-300 group-hover:text-opacity-100"
+          }
+        >
+          {header.toUpperCase()}
+        </h5>
+        <FaPlus
+          size="12"
+          className="text-accent text-opacity-90 ml-auto my-auto"
+        />
+      </div>
 
-            { expanded && 
-                selections && 
-                selections.map((selection)=> <TopicSelection selection={selection} /> )    
-            }
-        </div>
-    )
-}
+      {expanded &&
+        selections &&
+        selections.map((selection) => <TopicSelection selection={selection} />)}
+    </div>
+  );
+};
 
 interface chevronProps {
-    expanded : boolean
+  expanded: boolean;
 }
 
-const ChevronIcon = ({expanded} : chevronProps) => {
-    const chevClass = 'text-accent text-opacity-90 my-auto mr-1';
-    return expanded ? (
-      <FaChevronDown size='8' className={chevClass} />
-    ) : (
-      <FaChevronRight size='8' className={chevClass} />
-    );
-  };
-const TopicSelection = ({ selection } : {selection:string}) => (
-    <div className='dropdown-selection'>
-        <BsHash size='24' className='text-gray-400' />
-        <h5 className='dropdown-selection-text'>{selection}</h5>
-    </div>
+const ChevronIcon = ({ expanded }: chevronProps) => {
+  const chevClass =
+    "text-accent text-opacity-90 my-auto mr-1 group-hover:text-gray-300 group-hover:text-opacity-100";
+  return expanded ? (
+    <FaChevronDown size="8" className={chevClass} />
+  ) : (
+    <FaChevronRight size="8" className={chevClass} />
+  );
+};
+const TopicSelection = ({ selection }: { selection: string }) => (
+  <div className="dropdown-selection">
+    <RiHashtag size="24" className="text-gray-400" />
+    <h5 className="dropdown-selection-text">{selection}</h5>
+  </div>
 );
 const ChannelBlock = () => (
-    <div className='channel-block'>
-        <h5 className='channel-block-text'>Channels</h5>
-    </div>
-)
+  <div className="channel-block">
+    <h5 className="channel-block-text">Channels</h5>
+  </div>
+);
 
-export default ChannelBar
+export default ChannelBar;

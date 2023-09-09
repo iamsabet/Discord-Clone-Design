b23e3224383bae195266adf33e24d287f3b47e00
iamsabet <iamsabet@gmail.com>
2023-09-09 18:48:47 +0330

    Channels header(Server Name) mocked options on click + then channels action buttons design on show + then selected channels + servers

--- src/components/ChannelBar/ChannelsHeader.tsx
+++ src/components/ChannelBar/ChannelsHeader.tsx
@@ -1,6 +1,11 @@
+import { FaChevronDown } from "react-icons/fa6";
+
 const ChannelsHeader = () => (
   <div className="channel-block">
-    <h5 className="channel-block-text">Channels</h5>
+    <h6 className="channel-block-text text-ellipsis line-clamp-1">
+      Server Name
+    </h6>
+    <FaChevronDown size="14" className={`chevron`} />
   </div>
 );
 

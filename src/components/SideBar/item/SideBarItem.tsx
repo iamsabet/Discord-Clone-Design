49c1692f60a6a4471ae7ac1515c68aa06ce07b2e
iamsabet <iamsabet@gmail.com>
2023-09-11 18:27:56 +0330

    fix tooltips hide

--- src/components/SideBar/item/SideBarItem.tsx
+++ src/components/SideBar/item/SideBarItem.tsx
@@ -1,22 +1,18 @@
 import Pill from "./Pill";
 
 interface Props {
-    icon: React.ReactNode;
-    text : string
-}   
-
-const SideBarItem = ({icon, text='tooltip'}: Props) => {
-  return (
-    <div className="sidebar-group group relative">
-        <Pill />
-        <div className="sidebar-icon">
-          {icon}
-        </div>
-        <span className="sidebar-tooltip group-hover:scale-100">
-            {text} 💡
-        </span>
-    </div>
-  )
+  icon: React.ReactNode;
+  text: string;
 }
 
-export default SideBarItem
+const SideBarItem = ({ icon, text = "tooltip" }: Props) => {
+  return (
+    <div className="sidebar-group group relative">
+      <Pill />
+      <div className="sidebar-icon">{icon}</div>
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    </div>
+  );
+};
+
+export default SideBarItem;
